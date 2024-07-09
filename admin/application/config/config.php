2a2fddfbd9b8af57a9d99bf68c80e66a23147bd9
rahmantofauzy <rahmantofauzy@gmail.com>
2024-07-09 16:00:01 +0700

    Mengubah base_url

--- admin/application/config/config.php
+++ admin/application/config/config.php
@@ -24,8 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'https://www.dery.projectpdg.my.id/';
-//$config['base_url'] = '';
+$config['base_url'] = 'https://www.dery.projectpdg.my.id/admin/';
+//$config['base_url'] = 'http://localhost/dery/admin/';
 
 /*
 |--------------------------------------------------------------------------
